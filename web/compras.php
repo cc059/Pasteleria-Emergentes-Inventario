c4ea140164223c8fe829747128eb1672e5b38093
carolina <carolina.rodriguez@catolica.edu.sv>
2019-03-15 20:19:48 -0600

    Creación

---
+++ web/compras.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<title>Cakes Bakery Restaurants Category Bootstrap Responsive Web Template | Contact Us :: W3layouts</title>
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8" />
+	<meta name="keywords" content="Cakes Bakery Services Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- //Meta tag Keywords -->
+
+	<!-- Custom-Files -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+	<!-- Style-CSS -->
+	<link rel="stylesheet" href="css/fontawesome-all.css">
+	<!-- Font-Awesome-Icons-CSS -->
+	<!-- //Custom-Files -->
+
+	<!-- Web-Fonts -->
+	<link href="//fonts.googleapis.com/css?family=Oxygen:300,400,700&amp;subset=latin-ext" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese"
+	    rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Pacifico&amp;subset=cyrillic,latin-ext,vietnamese" rel="stylesheet">
+    <!-- //Web-Fonts -->
+    <!-- css de tabla -->
+    <style>
+#customers {
+  font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 95%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #4CAF50;
+  color: white;
+}
+</style>
+ <!-- css de tabla termina -->
+
+</head>
+
+<body>
+	<div class="mian-content">
+		<!-- header -->
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-light">
+				<div class="logo text-left">
+					<h1>
+						<a class="navbar-brand" href="index.php">
+							<img src="images/logo.png" alt="" class="img-fluid">Cakes Bakery</a>
+					</h1>
+				</div>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+				    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon">
+
+					</span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-lg-auto text-lg-right text-center">
+						<li class="nav-item active">
+							<a class="nav-link" href="index.php">Home
+								<span class="sr-only">(current)</span>
+							</a>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+							    aria-haspopup="true" aria-expanded="false">
+								Compras
+							</a>
+							<div class="dropdown-menu text-lg-left text-center" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item link"  href="inventario_producto.php">Compras</a>
+								<a class="dropdown-item link"  href="inventario_producto.php">Proveedores</a>
+								<a class="dropdown-item link"  href="inventario_producto.php">Inventario Producto</a>
+								<a class="dropdown-item link"  href="inventario_producto.php">Inventario Materia Prima</a>
+
+							</div>
+						</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+							    aria-haspopup="true" aria-expanded="false">
+								Ventas
+							</a>
+							<div class="dropdown-menu text-lg-left text-center" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item link"  href="inventario_producto.php">Compras</a>
+								<a class="dropdown-item link"  href="inventario_producto.php">Proveedores</a>
+								<a class="dropdown-item link"  href="inventario_producto.php">Inventario Producto</a>
+								<a class="dropdown-item link"  href="inventario_producto.php">Inventario Materia Prima</a>
+
+							</div>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="gallery.html">Gallery</a>
+						</li>
+											<!-- menu contactanos -->
+						<li class="nav-item">
+							<a class="nav-link" href="contact.html">Contáctanos</a>
+						</li>
+					</ul>
+					<!-- menu button -->
+					<div class="menu">
+						<a href="#" class="navicon"></a>
+						<div class="toggle">
+							<ul class="toggle-menu list-unstyled">
+								<li>
+									<a href="index.html">Index Page</a>
+								</li>
+								<li>
+									<a class="scroll" href="#products">New Products</a>
+								</li>
+								<li>
+									<a href="gallery.html">Latest Cakes</a>
+								</li>
+								<li>
+									<a class="scroll" href="#order">Order Cake</a>
+								</li>
+								<li>
+									<a class="scroll" href="#faqs">Faqs</a>
+								</li>
+								<li>
+									<a href="contact.html">Contact Us</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<!-- //menu button -->
+				</div>
+			</nav>
+		</header>
+		<!-- //header -->
+
+		<!-- banner 2 -->
+		<div class="banner2-w3ls">
+
+		</div>
+		<!-- //banner 2 -->
+	</div>
+	<!-- main -->
+	<!-- page details -->
+	<div class="breadcrumb-agile">
+		<nav aria-label="breadcrumb">
+			<ol class="breadcrumb m-0">
+				<li class="breadcrumb-item">
+					<a href="index.html">Home</a>
+				</li>
+			</ol>
+		</nav>
+	</div>
+	<!-- //page details -->
+
+	<!-- contact page -->
+	<div class="address py-5">
+		<div class="container py-xl-5 py-lg-3">
+			<div class="title text-center mb-5">
+				<h2 class="text-dark mb-2">Compras</h2>
+				<p>Registra tus compras.</p>
+			</div>
+			<div class="row address-row">
+				<div class="col-lg-6 address-right">
+					<div class="address-info wow fadeInRight animated" data-wow-delay=".5s">
+                    <h4 class="font-weight-bold mb-3">Repostería </h4>
+						<p>Para endulzarte el día</p>
+					</div>
+					<div class="address-info address-mdl wow fadeInRight animated" data-wow-delay=".7s">
+						<h4 class="font-weight-bold mb-3">Pasteles </h4>
+						<p>Chocolate</p>
+						<p>Caramelo</p>
+					</div>
+				</div>
+				<div class="col-lg-6 address-left mt-lg-0 mt-5">
+					<div class="address-grid">
+						<h4 class="font-weight-bold mb-3">Crea tu compra</h4>
+						<form action="#" method="post">
+                        <div class="form-group">
+                                Proveedor
+                                <select name="Proveedor">
+                                        <option value="value1">Carolina Rodriguez</option> 
+                                        <option value="value2" selected>Eduardo Noyola</option>
+                                 </select>
+                            </div>
+                            <div class="form-group">
+                                Empleado
+                                <select name="Empleado">
+                                        <option value="value1">Victor Ramirez</option> 
+                                        <option value="value2" selected>Joel Figueroa</option>
+                                 </select>
+                                 <br/><br/>
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Fecha" name="fecha_compra" required="">
+							</div>
+							<div class="form-group">
+								<input type="email" class="form-control" placeholder="Hora" name="hora_compra" required="">
+							</div><br/>
+							Tipo Pago
+                                <select name="TipoPago">
+                                        <option value="value1">Efectivo</option> 
+                                        <option value="value2" selected>Crédito</option>
+                                 </select><br/><br/>
+                                 Producto
+                                <select name="Producto">
+                                        <option value="value1">Harina</option> 
+                                        <option value="value2" selected>Azúcar Morena</option>
+                                        <option value="value3" selected>Leche</option>
+                                 </select>
+                                 <br/><br/>
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Cantidad" name="cantidad_compra" required="">
+							</div>
+							<div class="form-group">
+								<input type="email" class="form-control" placeholder="Precio" name="precio_compra" required="">
+							</div>
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Descuento" name="descuento_compra" required="">
+							</div><br/>
+							</div>
+                            <input type="submit" value="Comprar">
+						</form>
+					</div>
+                </div><br/><br/>
+
+<table id="customers">
+  <tr>
+    <th>Producto</th>
+    <th>Cantidad</th>
+    <th>Precio</th>
+    <th>Total</th>
+  </tr>
+  <tr>
+    <td>Huevos</td>
+    <td>50</td>
+    <td>0.10</td>
+    <td>5.00</td>
+  </tr>
+
+</table>
+			</div>
+		</div>
+	</div>
+	<!-- //contact page -->
+
+
+	<!-- footer -->
+	<footer class="text-center py-sm-4 py-3">
+		<div class="container py-xl-5 py-3">
+			<div class="w3l-footer footer-social-agile mb-4">
+				<ul class="list-unstyled">
+					<li>
+						<a href="#">
+							<i class="fab fa-facebook-f"></i>
+						</a>
+					</li>
+					<li class="mx-1">
+						<a href="#">
+							<i class="fab fa-twitter"></i>
+						</a>
+					</li>
+					<li>
+						<a href="#">
+							<i class="fab fa-dribbble"></i>
+						</a>
+					</li>
+					<li class="ml-1">
+						<a href="#">
+							<i class="fab fa-vk"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+			<!-- copyright -->
+			<p class="copy-right-grids text-light my-lg-5 my-4 pb-4">© 2018 Cake Bakery. All Rights Reserved | Design by
+				<a href="https://w3layouts.com/" target="_blank">W3layouts</a>
+			</p>
+			<!-- //copyright -->
+		</div>
+		<!-- chef -->
+		<img src="images/chef.png" alt="" class="img-fluid chef-style" />
+		<!-- //chef -->
+	</footer>
+	<!-- //footer -->
+
+
+	<!-- Js files -->
+	<!-- JavaScript -->
+	<script src="js/jquery-2.2.3.min.js"></script>
+	<!-- Default-JavaScript-File -->
+
+	<!-- menu-js -->
+	<script>
+		$('.navicon').on('click', function (e) {
+			e.preventDefault();
+			$(this).toggleClass('navicon--active');
+			$('.toggle').toggleClass('toggle--active');
+		});
+	</script>
+	<!-- //menu-js -->
+
+	<!-- smooth scrolling -->
+	<script src="js/SmoothScroll.min.js"></script>
+	<!-- move-top -->
+	<script src="js/move-top.js"></script>
+	<!-- easing -->
+	<script src="js/easing.js"></script>
+	<!--  necessary snippets for few javascript files -->
+	<script src="js/cakes-bakery.js"></script>
+
+	<script src="js/bootstrap.js"></script>
+	<!-- Necessary-JavaScript-File-For-Bootstrap -->
+
+	<!-- //Js files -->
+
+</body>
+
+</html>
