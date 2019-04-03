fdc37a3d49c83033061ee6488cf37c54a7937525
Sultanes25 <42598016+Sultanes25@users.noreply.github.com>
2019-04-02 21:47:44 -0600

    Update campos_inventario_producto.php

--- web/clases/campos_inventario_producto.php
+++ web/clases/campos_inventario_producto.php
@@ -22,7 +22,42 @@
         }
         function guardar(){
             if($this->id_producto>0){
-            $sql="UPDATE inventario_producto SET producto='{$this->producto}',precio='{$this->precio}', cantidad='{$this->cantidad}, id_categoria='{$this->id_categoria}', activo='{$this->activo}' WHERE id_producto='{$this->id_producto}';";
+                $sql="UPDATE inventario_producto SET producto='{$this->producto}',precio={$this->precio}, cantidad={$this->cantidad}, id_categoria={$this->id_categoria}, activo='{$this->activo}' WHERE id_producto={$this->id_producto};";
+                mysqli_query(conexion::obtenerInstancia(), $sql);
+                else{
+                $sql="INSERT INTO inventario_producto(producto,precio,cantidad,categoria,activo) VALUES('{$this->producto}',{$this->precio},{$this->cantidad},{$this->id_categoria},'{$this->activo}';";
+                mysqli_query(conexion::obtenerInstancia(),$sql);
+                }
+            }
+        }
+        static function listarInventarioProd(){
+            $productos=array();
+            $result=0;
+            $sql="SELECT * FROM inventario_producto";
+            $rs=mysqli_query(conexion::obtenerInstancia(),$sql);
+            if(){
+                while($fila = mysqli_fetch_assoc($rs)){
+                    $productos[]=$fila;
+                }
+            }
+            return $productos;
+        }
+        function eliminar($id_producto){
+            $sql="DELETE FROM inventario_producto WHERE id_producto=($id_producto)";
+            myqli_query(conexion::obtenerInstancia(),$sql);
+        }
+        function modificar($id_producto){
+            $sql="SELECT * FROM inventario_producto WHERE  id_producto= ($id_producto)";
+            $rs=mysqli_query(conexion::obtenerInstancia(), $sql);
+            if(mysqli_num_rows($rs)>0){
+                $fila=mysqli_fetch_assoc($rs);
+                $this->id_producto=$fila{'id_producto'};
+                $this->producto=$fila{'producto'};
+                $this->precio=$fila{'precio'};
+                $this->cantidad=$fila{'cantidad'};
+                $this->id_categoria=$fila{'id_categoria'};
+                $this->activo=$fila{'activo'};
+
             }
         }
     }
